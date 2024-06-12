10e17f230c04ae5ce84255139de07fb64e459227
somesetty manideep <developer@dt7solutions.com>
2024-06-12 16:53:46 +0530

    update contact page

--- fintech/app/urls.py
+++ fintech/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index,contactus
 
 
 
 urlpatterns = [
     path('',index,name='index'),
+    path('contactus/',contactus,name='contactus'),
 ]
--- fintech/app/views.py
+++ fintech/app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'uifiles/index.html')
+
+def contactus(request):
+    return render(request, 'uifiles/contact.html')
